64dcd5395f2e96b71095950ef5eaa9eda4dd33b3
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-07-03 09:30:27 +0900

    add build shell

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+GOOS=linux GOARCH=386 go build -o gradle-update-notifier_linux_386
+GOOS=linux GOARCH=amd64 go build -o gradle-update-notifier_linux_amd64
+GOOS=windows GOARCH=386 go build -o gradle-update-notifier_windows_386
+GOOS=windows GOARCH=amd64 go build -o gradle-update-notifier_windows_amd64
+GOOS=darwin GOARCH=386 go build -o gradle-update-notifier_darwin_386
+GOOS=darwin GOARCH=amd64 go build -o gradle-update-notifier_darwin_386_amd64
