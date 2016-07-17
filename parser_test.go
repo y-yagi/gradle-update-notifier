c33cc381ed92a806bdada99f490137bd31134cee
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-07-17 15:13:04 +0900

    add test for parser

---
+++ parser_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func TestParseSuccess(t *testing.T) {
+	jsonData, _ := ioutil.ReadFile("testdata/normal.json")
+	report, err := parse(jsonData)
+	if err != nil {
+		t.Errorf("Get error: %v", err)
+	}
+
+	if len(report.Outdated.Dependencies) != 1 {
+		t.Errorf("Expect %v, but %v", 1, len(report.Outdated.Dependencies))
+	}
+}
+
+func TestParseFailed(t *testing.T) {
+	jsonData, _ := ioutil.ReadFile("testdata/invalid.json")
+	_, err := parse(jsonData)
+	if err == nil {
+		t.Errorf("Expected parse error. But not")
+	}
+}
