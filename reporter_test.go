0a06a70bbea4efde9280061a05e2e6d7b7950c6d
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-08-05 08:54:28 +0900

    add test for generate issue body

---
+++ reporter_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func TestGenerateIssueBody(t *testing.T) {
+	jsonData, _ := ioutil.ReadFile("testdata/normal.json")
+	report, _ := parse(jsonData)
+	releaseNotes := []ReleaseNote{ReleaseNote{Package: "com.google.firebase:firebase-core", Url: "https://firebase.google.com/support/releases"}, ReleaseNote{Package: "com.github.ben-manes:gradle-version-plugin", Url: ""}}
+
+	issueBody := generateIssueBody(report, releaseNotes)
+	expected := "* [ ] `com.github.ben-manes:gradle-versions-plugin:0.13.0`([Release Note](https://firebase.google.com/support/releases))\n* [ ] `com.google.firebase:firebase-core:9.4.0`\n"
+
+	if issueBody != expected {
+		t.Errorf("Expect \n%v\n, but \n%v", expected, issueBody)
+	}
+}
