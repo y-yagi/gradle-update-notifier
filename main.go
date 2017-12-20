185396c0f8eff3682220892cee668b39c30a145e
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-12-21 06:58:40 +0900

    Bump version

--- main.go
+++ main.go
@@ -82,7 +82,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "gradle-update-notifier"
 	app.Usage = "notify gradle update"
-	app.Version = "0.1.0"
+	app.Version = "0.2.0"
 	app.Flags = commandFlags()
 
 	app.Action = func(c *cli.Context) error {
